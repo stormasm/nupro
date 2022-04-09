a32bd188d80d1423a91800edb4220c9c8b74b30c
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-04-09 09:32:06 -0700

    mw

--- examples/readb01.rs
+++ examples/readb01.rs
@@ -10,25 +10,6 @@ struct Person {
 
 fn main() -> Result<()> {
     let conn = Connection::open("person01.db")?;
-    /*
-        conn.execute(
-            "CREATE TABLE person (
-                      id              INTEGER PRIMARY KEY,
-                      name            TEXT NOT NULL,
-                      data            BLOB
-                      )",
-            [],
-        )?;
-        let me = Person {
-            id: 0,
-            name: "Steven".to_string(),
-            data: None,
-        };
-        conn.execute(
-            "INSERT INTO person (name, data) VALUES (?1, ?2)",
-            params![me.name, me.data],
-        )?;
-    */
     let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
     let person_iter = stmt.query_map([], |row| {
         Ok(Person {
