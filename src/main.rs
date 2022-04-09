f0e06655c4d69515eb45226cf4389219e83ca70c
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-04-08 21:08:54 -0700

    main.rs is up and running with a simple example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 mod delimited;
+use delimited::from_delimited_data;
+use nu_protocol::{Config, PipelineData, Span};
 
 fn main() {
-    println!("Hello, world!");
+    let config: Config = Config::default();
+    let span: Span = Span::test_data();
+    let input: PipelineData = PipelineData::new(span);
+    let result = from_delimited_data(false, ',', input, span, &config);
+    println!("{:?}", result);
 }
