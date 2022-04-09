c805bfa8cda3d895ff81682ba90b62152458444f
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-04-08 21:46:36 -0700

    mw

---
+++ examples/ex00.rs
@@ -0,0 +1,11 @@
+//mod delimited;
+use nu_protocol::{Config, PipelineData, Span};
+use nupro::delimited::from_delimited_data;
+
+fn main() {
+    let config: Config = Config::default();
+    let span: Span = Span::test_data();
+    let input: PipelineData = PipelineData::new(span);
+    let result = from_delimited_data(false, ',', input, span, &config);
+    println!("{:?}", result);
+}
